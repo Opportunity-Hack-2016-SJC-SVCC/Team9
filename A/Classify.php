11c5c36e56379344acbb60f7dc6beb8c46134366
shibunath <shibunathshanker@ymail.com>
2016-09-25 10:21:12 -0700

    Changes

--- A/Classify.php
+++ A/Classify.php
@@ -1,3 +1,94 @@
+
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>NPO DATA FILTERING</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Theme CSS -->
+    <link href="css/grayscale.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+
+</head>
+
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                Menu <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand page-scroll" href="#page-top">
+                <i class="fa fa-play-circle"></i> <span class="light">NPO DATA FILTERING</span>
+            </a>
+
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+            <ul class="nav navbar-nav">
+                <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#impact">Impact Index</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#business">Business Indicators</a>
+                </li>
+
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+
+<!-- Intro Header -->
+<header class="intro">
+    <div class="intro-body">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <h1>CLASSIFICATION</h1>
+                    <p class="intro-text">NPO Data Classification
+
+                Classified as
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
 <?php
 
 /**
@@ -165,16 +256,18 @@ if(!empty($_GET['mission_statement']))
     $mission_statement=$_GET['mission_statement'];
 
     if(strpos($mission_statement,"mobile")!==false) {
-        
-        $product = "sss";
+
+     //   $product = "sss ";
+       // echo $product;
     }
 
-$agr=['food','crop','plant'];
-   $cooking=['stoves','fuel','oil'];
-$healthcare=['eyeglasses','surgery','treatment'];
+$agr=['food','crop','technology'];
+   $cooking=['stoves','technology','oil'];
+$healthcare=['eyeglasses','technology','treatment'];
 
     for($x=0;$x<count($agr);$x++)
     {
+
     if(strpos($mission_statement,$agr[$x])!==false)
     {
         $product="agriculture";
@@ -184,6 +277,7 @@ $healthcare=['eyeglasses','surgery','treatment'];
 
     if($product == "") {
         for ($x = 0; $x < count($cooking); $x++) {
+
             if (strpos($mission_statement, $cooking[$x]) !== false) {
                 $product = "cooking";
                 break;
@@ -200,7 +294,7 @@ $healthcare=['eyeglasses','surgery','treatment'];
         }
     }
 
-
+echo $product;
 }
 else
 {
@@ -210,3 +304,5 @@ else
 
 
 ?>
+</body>
+</html>
